262181eb3b73575dc8ab15ab22c6a5b49919d5a0
Kiselev Danil <dabikis2855@gmail.com>
2023-04-24 19:53:27 +0600

    [+] role view

---
+++ src/main/java/com/example/byte_bank/view/RoleView.java
@@ -0,0 +1,15 @@
+package com.example.byte_bank.view;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleView {
+    private int id;
+    private String name;
+}
