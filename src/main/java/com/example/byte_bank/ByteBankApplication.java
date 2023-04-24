e923e78b4c0f8d62f604ad2b168f2c344a7a5296
Kiselev Danil <dabikis2855@gmail.com>
2023-04-24 23:37:08 +0600

    [+] roles existence check

--- src/main/java/com/example/byte_bank/ByteBankApplication.java
+++ src/main/java/com/example/byte_bank/ByteBankApplication.java
@@ -1,13 +1,38 @@
 package com.example.byte_bank;
 
+import com.example.byte_bank.entity.RoleEntity;
+import com.example.byte_bank.entity.UserEntity;
+import com.example.byte_bank.repository.RoleRepository;
+import com.example.byte_bank.repository.UserRepository;
+import com.example.byte_bank.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class ByteBankApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ByteBankApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner roles(RoleRepository roleRepository) {
+        return args -> {
+            Optional<RoleEntity> userRoleEntity = roleRepository.findByName("ROLE_USER");
+            Optional<RoleEntity> adminRoleEntity = roleRepository.findByName("ROLE_ADMIN");
+            if (userRoleEntity.isEmpty()) {
+                RoleEntity userRole = new RoleEntity(1, "ROLE_USER");
+                roleRepository.save(userRole);
+            }
+            if (adminRoleEntity.isEmpty()) {
+                RoleEntity adminRole = new RoleEntity(2, "ROLE_ADMIN");
+                roleRepository.save(adminRole);
+            }
+        };
     }
 
 }
