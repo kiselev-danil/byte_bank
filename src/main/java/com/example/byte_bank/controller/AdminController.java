1937772ed603ff9ad89f57099b2019415d20cd4f
Kiselev Danil <dabikis2855@gmail.com>
2023-04-24 16:02:47 +0600

    [+] controller for admins

---
+++ src/main/java/com/example/byte_bank/controller/AdminController.java
@@ -0,0 +1,48 @@
+package com.example.byte_bank.controller;
+
+import com.example.byte_bank.service.AdminService;
+import com.example.byte_bank.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/admin")
+@AllArgsConstructor
+public class AdminController {
+
+    AdminService adminService;
+
+    @PostMapping(path = "/deleteById/{userId}")
+    public ResponseEntity<?> deleteUserById(@PathVariable int userId) {
+        adminService.deleteUserById(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/deleteByUsername/{userName}")
+    public ResponseEntity<?> deleteUserByUsername(@PathVariable String userName) {
+        adminService.deleteUserByUsername(userName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/deleteAllUsers")
+    public ResponseEntity<?> deleteAllUsers() {
+        adminService.deleteAllUsers();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping(path = "/creditBytesById/{userId}")
+    public ResponseEntity<?> creditToUserById(@RequestBody int bytesNumber, @PathVariable int userId) {
+        adminService.creditToUserById(bytesNumber, userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping(path = "/creditBytesByUsername/{userName}")
+    public ResponseEntity<?> creditToUserByUsername(@RequestBody int bytesNumber, @PathVariable String userName) {
+        adminService.creditToUserByUsername(bytesNumber, userName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+}
