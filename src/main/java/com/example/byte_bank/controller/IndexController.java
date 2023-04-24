9d8563a779d1a6df2c5530319c2f1a0534eb14d9
Kiselev Danil <dabikis2855@gmail.com>
2023-04-24 23:39:44 +0600

    [+] annotations for index controller

--- src/main/java/com/example/byte_bank/controller/IndexController.java
+++ src/main/java/com/example/byte_bank/controller/IndexController.java
@@ -1,11 +1,17 @@
 package com.example.byte_bank.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path = "/")
+@AllArgsConstructor
 public class IndexController {
-    @GetMapping(path = "/")
+    @GetMapping(path = "")
     public ResponseEntity<?> redirector() {
 
         return new ResponseEntity<>(HttpStatus.OK);
