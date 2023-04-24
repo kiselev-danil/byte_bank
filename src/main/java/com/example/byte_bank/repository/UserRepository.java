7e2f27223811ce81677cb52fae364e34082f4f78
Kiselev Danil <dabikis2855@gmail.com>
2023-04-24 14:24:00 +0600

    [*] query fix

--- src/main/java/com/example/byte_bank/repository/UserRepository.java
+++ src/main/java/com/example/byte_bank/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.example.byte_bank.repository;
 
 import com.example.byte_bank.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    public Optional<UserEntity> findByLogin(String login);
+
 }
