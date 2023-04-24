849c2b3d36f85de4f006b69f66417179c17e0d8b
Kiselev Danil <dabikis2855@gmail.com>
2023-04-24 19:54:14 +0600

    [*] now converts roles

--- src/main/java/com/example/byte_bank/service/UserConverter.java
+++ src/main/java/com/example/byte_bank/service/UserConverter.java
@@ -1,11 +1,17 @@
 package com.example.byte_bank.service;
 
+import com.example.byte_bank.entity.RoleEntity;
 import com.example.byte_bank.entity.UserEntity;
+import com.example.byte_bank.view.RoleView;
 import com.example.byte_bank.view.UserView;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+@Service
 public class UserConverter {
     public List<UserView> ConvertArrayEntityToView(List<UserEntity> list) {
         List<UserView> result = new ArrayList<>();
@@ -16,6 +22,12 @@ public class UserConverter {
     }
 
     public UserView convertUserEntityToView(UserEntity entity) {
-        return new UserView(entity.getId(), entity.getLogin(), entity.getBalance());
+        Set<RoleEntity> rolesEtities = entity.getRoles();
+        Set<RoleView> rolesViews = new HashSet<>();
+        for (RoleEntity role : rolesEtities) {
+            RoleView rv = new RoleView(role.getId(), role.getName());
+            rolesViews.add(rv);
+        }
+        return new UserView(entity.getId(), entity.getLogin(), entity.getBalance(), rolesViews);
     }
 }
